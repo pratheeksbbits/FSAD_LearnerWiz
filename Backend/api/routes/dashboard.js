e4094e1ec38bf8daf5334ac6d4bdf12da88f1185
Sarthak Agrawal <sarthak.aggarwal1234@gmail.com>
2024-05-06 00:03:35 +0530

    Dashboard API + Documentation

---
+++ Backend/api/routes/dashboard.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const Language = require("../models/language");
+const UserLanguageMapping = require("../models/user_language_mapping")
+const mongoose = require('mongoose');
+
+router.get('/:email', async (req, res) => {
+    const email = req.params.email;
+    try {
+        const languageData = await Language.find();
+        const wordList = await UserLanguageMapping.find({ email });
+        const data  = languageData.map((value)=>{
+            return {
+                code: value.code,
+                languageId: value.languageId,
+                progress: (wordList.filter((word)=> word.languageId == value.languageId).length)/value.content.length,
+            }
+        })
+        res.status(200).json({data: data});
+    } catch (error) {
+        console.error('Error fetching language content:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+module.exports = router;
